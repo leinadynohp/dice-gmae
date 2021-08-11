25f3c0823db2c21fab9451285e10c7aeb45abc20
Leinad Kim <leinadynohp@gmail.com>
2021-08-11 20:09:29 +0900

    button action & js modification

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-var randomNumber1={};
-var randomNumber2={};
+function diceRoll() {
+    var randomNumber1 = Math.ceil(Math.random()*6);
+    var randomNumber2 = Math.ceil(Math.random()*6);
 
-function reload() {
-    randomNumber1=Math.ceil(Math.random()*6);
-    randomNumber2=Math.ceil(Math.random()*6); 
+    var diceImage1 = "./images/dice" + randomNumber1 + ".png";
+    var diceImage2 = "./images/dice" + randomNumber2 + ".png";
 
-    if (randomNumber1===1) {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice1.png");
-    } else if (randomNumber1===2) {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice2.png");
-    } else if (randomNumber1===3) {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice3.png");       
-    } else if (randomNumber1===4) {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice4.png");
-    } else if (randomNumber1===5) {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice5.png");
-    } else {
-        document.querySelector(".dice-image-1").setAttribute("src", "./images/dice6.png");
-    }
-
-    if (randomNumber2===1) {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice1.png");
-    } else if (randomNumber2===2) {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice2.png");
-    } else if (randomNumber2===3) {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice3.png");
-    } else if (randomNumber2===4) {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice4.png");
-    } else if (randomNumber2===5) {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice5.png");
-    } else {
-        document.querySelector(".dice-image-2").setAttribute("src", "./images/dice6.png");
-    }
+    document.querySelector('.dice-image-1').setAttribute('src', diceImage1);
+    document.querySelector('.dice-image-2').setAttribute('src', diceImage2);
 
     if (randomNumber1 > randomNumber2) {
         document.querySelector("h1").innerHTML="<img src='./images/finish.png' width='100rem'> Player1 Won!"
     } else if (randomNumber1 < randomNumber2) {
-        document.querySelector("h1").innerHTML="Player2 Won <img src='./images/finish.png' width='100rem'>"
+        document.querySelector("h1").innerHTML="Player2 Won! <img src='./images/finish.png' width='100rem'>"
     } else {
-        document.querySelector("h1").innerHTML="<img src='./images/finish.png' width='100rem'> You Are Tie <img src='./images/finish.png' width='100rem'>"
+        document.querySelector("h1").innerHTML="<img src='./images/finish.png' width='100rem'> Draw! &nbsp <img src='./images/finish.png' width='100rem'>"
     }
 }
